ede4cd578e685749092b4974ae78805d8abc876d
J.Ting <J.Ting@LAPTOP-VGH5DCMD>
2018-12-30 21:01:16 +0800

    updated image but when add new image that is not uploaded previously into ip/img/profile folder..need to click back the viewprofile link only can view it..

--- src/java/member/UploadServlet.java
+++ src/java/member/UploadServlet.java
@@ -6,12 +6,12 @@
 package member;
 
 import bean.User;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,32 +19,30 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 import jdbc.JDBCUtility;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.annotation.MultipartConfig;
-import java.sql.DriverManager;
-import java.io.ByteArrayOutputStream;
-import java.util.Base64;
+
 /**
  *
  * @author J.Ting
  */
 @WebServlet(name = "UploadServlet", urlPatterns = {"/UploadServlet"})
-@MultipartConfig(maxFileSize = 16177215) 
+@MultipartConfig(location="C:\\Users\\J.Ting\\Documents\\NetBeansProjects\\ip\\web\\img\\profile",
+                 fileSizeThreshold=1024*1024*2, // 2MB
+                 maxFileSize=1024*1024*10,      // 50MB
+                 maxRequestSize=1024*1024*50)   // 50MB
 public class UploadServlet extends HttpServlet {
-  private JDBCUtility jdbcUtility;
+
+    private JDBCUtility jdbcUtility;
     private Connection con;
-    String driver = "com.mysql.jdbc.Driver";
+    
+     public void init() 
+    {
+        String driver = "com.mysql.jdbc.Driver";
 
         String dbName = "cash";
         String url = "jdbc:mysql://localhost/" + dbName + "?";
         String userName = "root";
         String password = "";
-    @Override
-    public void init() throws ServletException
-    {
-        
 
         jdbcUtility = new JDBCUtility(driver,
                                       url,
@@ -54,7 +52,10 @@ public class UploadServlet extends HttpServlet {
         jdbcUtility.jdbcConnect();
         con = jdbcUtility.jdbcGetConnection();
     }    
-
+    
+  
+    private static final String SAVE_DIR = "img";
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -64,91 +65,108 @@ public class UploadServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-  
-       protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        // gets values of text fields
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         
-        PrintWriter out=response.getWriter();
-        InputStream inputStream = null; // input stream of the upload file
-         
-        // obtains the upload file part in this multipart request
-        Part filePart = request.getPart("image");
-        if (filePart != null) {
-           
-            // obtains input stream of the upload file
-            inputStream = filePart.getInputStream();
-          
-        }
-         
-       
-        String message = null;  // message will be sent back to client
+        
         HttpSession session = request.getSession();
+        String login = ((User)session.getAttribute("memberprofile")).getLogin();
         
-        try {
-            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            con = DriverManager.getConnection(url, userName, password);
-            
-             String ulogin = ((User)session.getAttribute("memberprofile")).getLogin();
-            // constructs SQL statement
-            String sql = "UPDATE user SET image=? WHERE login=?";
-            PreparedStatement statement = con.prepareStatement(sql);
-                        
-            if (inputStream != null) {
-                // fetches input stream of the upload file for the blob column
-                statement.setBlob(1, inputStream);
-            }
-            statement.setString(2,ulogin);
-            // sends the statement to the database server
-            int row = statement.executeUpdate();
-            if(row>0)
-            {
-                session.setAttribute("uploadSuccess","Image has been successfully uploaded");
-            }
-            else
-            {
-               session.setAttribute("uploadSuccess","Image failed to upload");
-               response.sendRedirect(request.getContextPath()+"/memberprofile.jsp");
-            }
-        } catch (SQLException ex) {
-            message = "ERROR: " + ex.getMessage();
-            ex.printStackTrace();
-        } finally {
-            if (con != null) {
-                // closes the database connection
-                try {
-                    con.close();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-            }
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                byte[] buffer = new byte[4096];
-                int bytesRead = -1;
-                 
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);                  
-                }
-                 
-                byte[] imageBytes = outputStream.toByteArray();
-                String base64Image = Base64.getEncoder().encodeToString(imageBytes);
-                 
-                 
-                inputStream.close();
-                outputStream.close();
-            // StandardCharsets.UTF_8.name() > JDK 7
-            
-            User user = (User)session.getAttribute("memberprofile");
-            user.setImage(base64Image);
-            // sets the message in request scope
-            request.setAttribute("memberprofile", user);
-           
-            // redirect to member profile page
-            response.sendRedirect(request.getContextPath() + "/viewProfileServlet");  
+        String appPath = request.getServletContext().getRealPath("");
+        String savePath = appPath + File.separator + SAVE_DIR;
+        
+    
+        File fileSaveDir = new File(savePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdir();
         }
+        
+        String fileName = "";
+        for (Part part : request.getParts()) {
+            fileName = extractFileName(part);
+            part.write(fileName);
+        }
+        String sql="UPDATE user SET image = ? WHERE login = ?";
+       
+        try {                    
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, fileName);
+            preparedStatement.setString(2, login);
+          
+            preparedStatement.executeUpdate();
+        }
+	catch (SQLException ex)
+	{
+            while (ex != null)
+            {
+                System.out.println ("SQLState: " +
+                                 ex.getSQLState ());
+                System.out.println ("Message:  " +
+                                 ex.getMessage ());
+		System.out.println ("Vendor:   " +
+                                 ex.getErrorCode ());
+                ex = ex.getNextException ();
+		      System.out.println ("");
+            }
+            
+            System.out.println("Error connection to the database");
+	}
+	catch (java.lang.Exception ex)
+	{
+            ex.printStackTrace ();
+	}
+
+        User user = (User)session.getAttribute("memberprofile");
+        user.setImage(fileName);
+        session.setAttribute("memberprofile", user);
+        
+        response.sendRedirect("viewProfileServlet");
+        
+                
+    }
+    
+    /**
+     * Extracts file name from HTTP header content-disposition
+     */
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length()-1);
+            }
+        }
+        return "";
     }
 
-    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
     /**
      * Returns a short description of the servlet.
@@ -161,4 +179,5 @@ public class UploadServlet extends HttpServlet {
     }// </editor-fold>
 
 }
+
 
